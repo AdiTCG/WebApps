411526182c92795fbf8b7cee1f466fa3a7fd6a9c
Adi Soumitra Mondal <theair@Adis-MacBook-Air.local>
2019-08-24 12:29:34 +0530

    create employee forms

--- AngularCrud/src/app/app.module.ts
+++ AngularCrud/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularCrud/src/app/employee/create-employee.component.ts
+++ AngularCrud/src/app/employee/create-employee.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,8 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class CreateEmployeeComponent implements OnInit {
 
   constructor() { }
+  gender = 'male'
+  active = true
+  department = 'IT'
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'IT' },
+    { id: 3, name: 'Admin' },
+    { id: 5, name: 'NewDept' },
+    { id: 4, name: 'Payroll' }
+  ]
 
   ngOnInit() {
+  }
+  saveEmployee(empForm: NgForm): void {
+    console.log(empForm.value);
   }
 
 }
