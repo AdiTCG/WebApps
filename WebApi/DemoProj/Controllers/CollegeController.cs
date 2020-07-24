7d6116dbf1c1505dc0eb01b596514307e8787ae6
Adi Soumitra Mondal <soumitra.pro@gmail.com>
2020-07-24 15:25:28 +0530

    just added a class CollegeController. Inherited from AppController. Added a Get method.

---
+++ WebApi/DemoProj/Controllers/CollegeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DemoProj.Controllers
+{
+    public class CollegeController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Dept1", "Dept2" };
+        }
+    }
+}
