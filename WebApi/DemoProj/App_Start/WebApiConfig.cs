c0f7702205d91c4c8113d470e203a7fbaa117da9
Adi Soumitra Mondal <soumitra.pro@gmail.com>
2020-07-25 03:30:38 +0530

    Change the default url to access methods. Changed from "url/api/" to "url/default"

--- WebApi/DemoProj/App_Start/WebApiConfig.cs
+++ WebApi/DemoProj/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace DemoProj
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "default/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
