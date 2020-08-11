71cdb18abfd272af1d8abbe9e1371dbf58e811a5
Adi Soumitra Mondal <soumitra.pro@gmail.com>
2020-08-12 02:48:49 +0530

    Extracted interface for autofac.

---
+++ WebApi/DataAccessor/ICollegeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DemoProj.Models;
+using System.Data.Entity;
+
+namespace DataAccessor
+{
+    public interface ICollegeDbContext
+    {
+        DbSet<Department> Departments
+        {
+            get;
+            set;
+        }
+        DbSet<Student> Students
+        {
+            get;
+            set;
+        }
+    }
+}
