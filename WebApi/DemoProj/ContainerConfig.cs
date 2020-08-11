51c589e68522e3fee839fe867f5ef1dcf0e1de30
Adi Soumitra Mondal <soumitra.pro@gmail.com>
2020-08-12 02:52:10 +0530

    Added autofac from nuget packet manager. ontainerConfig is the one place registration for autofac.

---
+++ WebApi/DemoProj/ContainerConfig.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoProj
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+            return builder.Build();
+        }
+    }
+}
